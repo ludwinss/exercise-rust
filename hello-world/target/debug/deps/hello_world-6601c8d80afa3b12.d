1e4bc35730579e8b74b863a9e4a02e503337035c
Ludwin FM <ludwinss@gmail.com>
2024-11-11 22:28:58 -0400

    add gitignore

--- hello-world/target/debug/deps/hello_world-6601c8d80afa3b12.d
+++
@@ -1,5 +0,0 @@
-/home/mrmemo-dev/workspace/practice/rust/100-exercise-rust/hello-world/target/debug/deps/hello_world-6601c8d80afa3b12: src/main.rs
-
-/home/mrmemo-dev/workspace/practice/rust/100-exercise-rust/hello-world/target/debug/deps/hello_world-6601c8d80afa3b12.d: src/main.rs
-
-src/main.rs:
--- hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/home/mrmemo-dev/workspace/practice/rust/100-exercise-rust/hello-world/target/debug/hello-world: /home/mrmemo-dev/workspace/practice/rust/100-exercise-rust/hello-world/src/main.rs
